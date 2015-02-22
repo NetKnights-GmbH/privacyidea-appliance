40a235fcf05469d5a2b117dbca208137445c4eba
Cornelius Kölbel <cornelius.koelbel@netknights.it>
2015-02-22 09:42:12 +0100

    Copying first files from privacyidea 1.5

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#VERSION = "1.5dev9"
+VERSION = "2.0dev0"
+from setuptools import setup, find_packages
+import os
+import sys
+
+# Taken from kennethreitz/requests/setup.py
+package_directory = os.path.realpath(os.path.dirname(__file__))
+
+
+def get_file_contents(file_path):
+    """Get the context of the file using full path name."""
+    content = ""
+    try:
+        full_path = os.path.join(package_directory, file_path)
+        content = open(full_path, 'r').read()
+    except:
+        print >> sys.stderr, "### could not open file: %r" % file_path
+    return content
+
+
+setup(
+    name='2fa-appliance',
+    version=VERSION,
+    description='Appliance package for privacyIDEA: identity, multifactor authentication, '
+                'authorization, audit',
+    author='privacyidea.org',
+    license='AGPL v3',
+    author_email='cornelius@privacyidea.org',
+    url='http://www.privacyidea.org',
+    install_requires=["dialog"
+                      ],
+    scripts=['2fa-appliance/privacyidea-setup',
+             '2fa-appliance/privacyidea-setup-tui'],
+    include_package_data=True,
+    classifiers=["License :: OSI Approved :: "
+                 "GNU Affero General Public License v3",
+                 "Programming Language :: Python",
+                 "Development Status :: 5 - Production/Stable",
+                 "Topic :: Internet",
+                 "Topic :: Security",
+                 "Topic :: System ::"
+                 " Systems Administration :: Authentication/Directory"
+                 ],
+    zip_safe=False,
+    long_description=get_file_contents('README.md')
+)
