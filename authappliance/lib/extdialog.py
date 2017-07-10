f5247644f9b05824fc1acd5aa1e6407d638c60e2
Friedrich Weber <friedrich.weber@netknights.it>
2017-07-10 16:30:42 +0200

    LDAP Proxy: Use ``radiolist`` when possible
    Previously, we mimicked the radiolist using ``menu``, but there
    is no need to do that.
    
    Working on #28

--- authappliance/lib/extdialog.py
+++ authappliance/lib/extdialog.py
@@ -2,12 +2,11 @@ import dialog
 
 
 class ExtDialog(dialog.Dialog):
-    def choice(self, text, height=15, width=54, menu_height=7,
-               choices=None, current=None, current_template='{} (current)',
-               **kwargs):
+    def value_menu(self, text, height=15, width=54, menu_height=7,
+                   choices=None, **kwargs):
         """
-        Extended version of ``menu``. Given a list of value choices,
-        let the user choose one value. Additionally highlight the currently chosen item.
+        Extended version of ``menu``. Given a list of value choices, let the user choose a value.
+        This function may be handy because it allows to separate tags and return values.
 
         ``choices`` is a list of tuples like::
 
@@ -25,9 +24,6 @@ class ExtDialog(dialog.Dialog):
         :param width: see ``menu``
         :param menu_height: see ``menu``
         :param choices: A list of tuples [(return_value1, tag1, ...), ...]
-        :param current: The currently chosen value (the item will be highlighted)
-        :param current_template: A format string which is used to highlight the current item
-        :param kwargs:
         :return: The value specified in ``choices`` of the chosen item, or None if the user did not choose anything.
         """
         if choices is None:
@@ -38,13 +34,64 @@ class ExtDialog(dialog.Dialog):
         for choice in choices:
             return_value, tag = choice[0], choice[1]
             rest = choice[2:]
-            # if this happens to be the current choice, add a hint
-            if current == return_value:
-                tag = current_template.format(tag)
             # collect return value associated to the tag
             return_values[tag] = return_value
             processed_choices.append((tag,) + rest)
         code, result = self.menu(text, height, width, menu_height, processed_choices, **kwargs)
+        if code != self.DIALOG_OK:
+            return None
+        return return_values[result]
+
+    def value_radiolist(self, text, height=15, width=54, menu_height=7,
+                        choices=None, current=None,
+                        **kwargs):
+        """
+        Extended version of ``radiolist``. Given a list of value choices, let the user choose a value.
+        This function may be handy because it allows to separate tags and return values.
+
+        ``choices`` is a list of tuples like::
+
+            [(return_value1, tag1, item1),
+             (return_value2, tag2, item2),
+             ...]
+
+        or, in case ``item_help=True`` is passed::
+
+            [(return_value1, tag1, item1, help1),
+             ...]
+
+        In contrast to ``radiolist``, the current value is given in the parameter ``current``.
+        If ``current`` is not found in any choice, the first item is preselected.
+
+        :param text: see ``menu``
+        :param height: see ``menu``
+        :param width: see ``menu``
+        :param menu_height: see ``menu``
+        :param choices: A list of tuples [(return_value1, tag1, ...), ...]
+        :param current: The currently chosen value (the item will be highlighted)
+        :return: The value specified in ``choices`` of the chosen item, or None if the user did not choose anything.
+        """
+        if choices is None:
+            choices = []
+        assert len(choices) > 0
+        return_values = {}
+        # find out if we have to preselect the first choice
+        if all(choice[0] != current for choice in choices):
+            current = choices[0][0]
+        # preprocess ``choices`` to pass them to ``Dialog.menu``
+        processed_choices = []
+        for choice in choices:
+            return_value, tag = choice[0], choice[1]
+            rest = choice[2:]
+            # collect return value associated to the tag
+            return_values[tag] = return_value
+            # if this happens to be the current choice, preselect it
+            preselect = current == return_value
+            # the following line is somewhat weird: in case ``item_help=True`` is passed,
+            # the choices list needs to look like that:
+            # [(tag, desc, status, help), ...]
+            processed_choices.append((tag, rest[0], preselect) + rest[1:])
+        code, result = self.radiolist(text, height, width, menu_height, processed_choices, **kwargs)
         if code != self.DIALOG_OK:
             return None
         return return_values[result]
